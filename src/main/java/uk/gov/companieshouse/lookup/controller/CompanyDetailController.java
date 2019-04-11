df52ce7556ec7fa83b6cfd4997248bc4a2d9115c
sroberts2 <sroberts2@companieshouse.gov.uk>
2019-04-11 12:04:26 +0100

    added back button to details page

--- src/main/java/uk/gov/companieshouse/lookup/controller/CompanyDetailController.java
+++ src/main/java/uk/gov/companieshouse/lookup/controller/CompanyDetailController.java
@@ -1,7 +1,6 @@
 package uk.gov.companieshouse.lookup.controller;
 
 
-
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.springframework.web.util.UriTemplate;
 import uk.gov.companieshouse.lookup.exception.ServiceException;
 import uk.gov.companieshouse.lookup.model.CompanyDetail;
@@ -25,19 +25,24 @@ public class CompanyDetailController {
     private CompanyLookupService companyLookupService;
 
     @GetMapping
-    public String getCompanyDetail(
-        @ModelAttribute("companyDetail") @Valid CompanyDetail companyDetail, Model model) throws ServiceException {
+    public String getCompanyDetail(@RequestParam("forward") String forward,
+        @ModelAttribute("companyDetail") @Valid CompanyDetail companyDetail, Model model)
+        throws ServiceException {
         CompanyDetail company = companyLookupService
             .getCompanyProfile(companyDetail.getCompanyNumber());
         model.addAttribute("companyDetail", company);
 
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
+            .fromUriString("/company-lookup/search")
+            .queryParam("forward", forward);
+
+        model.addAttribute("backButton", uriComponentsBuilder.build().toUri());
         return "lookup/companyDetail";
     }
 
     @PostMapping
     public String postCompanyDetail(@RequestParam("forward") String forward,
-        @ModelAttribute("companyDetail") CompanyDetail companyDetail) {
-
+        @ModelAttribute("companyDetail") CompanyDetail companyDetail, Model model) {
         UriTemplate forwardURI = new UriTemplate(forward);
         return UrlBasedViewResolver.REDIRECT_URL_PREFIX +
             forwardURI.expand(companyDetail.getCompanyNumber()).toString();
