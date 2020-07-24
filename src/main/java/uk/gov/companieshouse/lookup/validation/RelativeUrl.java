af15f15f2a57d90ca4325db2d05037cf46716aef
dgroves-ch <dgroves@companieshouse.gov.uk>
2020-07-24 16:11:56 +0100

    Rewrite forward url validator based on rfc

--- src/main/java/uk/gov/companieshouse/lookup/validation/RelativeUrl.java
+++ src/main/java/uk/gov/companieshouse/lookup/validation/RelativeUrl.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = RelativeUrlValidator.class)
+@Constraint(validatedBy = UrlPathValidator.class)
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RelativeUrl {
