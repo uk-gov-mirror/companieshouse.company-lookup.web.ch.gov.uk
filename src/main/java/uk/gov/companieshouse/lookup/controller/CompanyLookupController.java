631aff2504914f218e81fd67ae381422383704eb
Simon Boyd <sboyd@companieshouse.gov.uk>
2021-04-26 12:09:41 +0100

    Add no-number link
    - add unit tests
    
    Resolves:
    BI-7388

--- src/main/java/uk/gov/companieshouse/lookup/controller/CompanyLookupController.java
+++ src/main/java/uk/gov/companieshouse/lookup/controller/CompanyLookupController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 import org.springframework.web.util.UriTemplate;
 import uk.gov.companieshouse.lookup.exception.InvalidRequestException;
@@ -23,10 +24,12 @@ import uk.gov.companieshouse.lookup.validation.ValidationError;
 import uk.gov.companieshouse.lookup.validation.ValidationHandler;
 
 @Controller
-@RequestMapping("/company-lookup/search")
+@RequestMapping("/company-lookup")
 public class CompanyLookupController {
 
     private static final String COMPANY_LOOKUP = "lookup/companyLookup";
+    private static final String INVALID_FORWARD_URL = "Invalid forward URL: [%s]";
+    public static final String NO_COMPANY_OPTION = "noCompanyOption";
 
     @Autowired
     private CompanyLookupService companyLookupService;
@@ -34,25 +37,42 @@ public class CompanyLookupController {
     @Autowired
     private ValidationHandler validationHandler;
 
-    @GetMapping
-    public String getCompanyLookup(@Valid ForwardUrl forward, BindingResult forwardResult, Model model) throws InvalidRequestException {
+    @GetMapping("/search")
+    public String getCompanyLookup(@Valid ForwardUrl forward, BindingResult forwardResult, Model model,
+        @RequestParam(name = NO_COMPANY_OPTION, required = false) String noCompanyOption) throws InvalidRequestException {
         if(forwardResult.hasErrors()) {
-            throw new InvalidRequestException(String.format("Invalid forward URL: [%s]", forward.getForward()));
+            throw new InvalidRequestException(String.format(INVALID_FORWARD_URL, forward.getForward()));
         }
         CompanyLookup companyLookup = new CompanyLookup();
         model.addAttribute("companyLookup", companyLookup);
+        model.addAttribute(NO_COMPANY_OPTION, noCompanyOption);
         return COMPANY_LOOKUP;
     }
+    
+    @GetMapping("/no-number")
+    public String getCompanyLookupNoCompany(@Valid ForwardUrl forward, BindingResult forwardResult,
+        Model model) throws InvalidRequestException {
+        if(forwardResult.hasErrors()) {
+            throw new InvalidRequestException(String.format(INVALID_FORWARD_URL, forward.getForward()));
+        }
+        UriTemplate forwardURI = new UriTemplate(forward.getForward());
+        
+        return UrlBasedViewResolver.REDIRECT_URL_PREFIX + forwardURI.expand("noCompany");
+    }
+    
 
-    @PostMapping
+    @PostMapping("/search")
     public String postCompanyLookup(@Valid ForwardUrl forward, BindingResult forwardResult,
-                                    @ModelAttribute("companyLookup") @Valid CompanyLookup companyLookup,
-                                    BindingResult bindingResult) throws InvalidRequestException, ServiceException {
+        @ModelAttribute("companyLookup") @Valid CompanyLookup companyLookup,
+        BindingResult bindingResult, Model model,
+        @RequestParam(name = NO_COMPANY_OPTION, required = false) String noCompanyOption)
+        throws InvalidRequestException, ServiceException {
         if(forwardResult.hasErrors()){
-            throw new InvalidRequestException(String.format("Invalid forward URL: [%s]", forward.getForward()));
+            throw new InvalidRequestException(String.format(INVALID_FORWARD_URL, forward.getForward()));
         }
 
         if (bindingResult.hasErrors()) {
+            model.addAttribute(NO_COMPANY_OPTION, noCompanyOption);
             return COMPANY_LOOKUP;
         }
 
@@ -66,6 +86,7 @@ public class CompanyLookupController {
             error.setMessageKey("company.not.found");
             validationErrors.add(error);
             validationHandler.bindValidationErrors(bindingResult, validationErrors);
+            model.addAttribute(NO_COMPANY_OPTION, noCompanyOption);
             return COMPANY_LOOKUP;
         }
 
