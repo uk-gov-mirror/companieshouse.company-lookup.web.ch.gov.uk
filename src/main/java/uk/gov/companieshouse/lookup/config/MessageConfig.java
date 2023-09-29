6796bb00b55134d9253716095a8f57460a7fd492
mchhatralia-ch <mchhatralia@companieshouse.gov.uk>
2023-09-29 11:09:06 +0100

    Refactor code to use common locale templates in common-web-java repo.

--- src/main/java/uk/gov/companieshouse/lookup/config/MessageConfig.java
+++ src/main/java/uk/gov/companieshouse/lookup/config/MessageConfig.java
@@ -1,7 +1,6 @@
 package uk.gov.companieshouse.lookup.config;
 
 import java.util.Locale;
-
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
