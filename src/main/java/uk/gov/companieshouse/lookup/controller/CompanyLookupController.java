f848818ea6bb331c92af85c83c8d5c6628734800
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2024-10-16 13:53:57 +0100

    AOAF-505: Autowired on constructor

--- src/main/java/uk/gov/companieshouse/lookup/controller/CompanyLookupController.java
+++ src/main/java/uk/gov/companieshouse/lookup/controller/CompanyLookupController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -37,6 +38,7 @@ public class CompanyLookupController {
 
     private final ValidationHandler validationHandler;
 
+    @Autowired
     public CompanyLookupController(CompanyLookupService companyLookupService, ValidationHandler validationHandler) {
         this.companyLookupService = companyLookupService;
         this.validationHandler = validationHandler;
