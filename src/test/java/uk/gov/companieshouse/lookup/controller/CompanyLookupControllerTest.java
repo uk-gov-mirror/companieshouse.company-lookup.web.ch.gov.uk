26f245eef7800ada9f6b082d5675caf2de8cef48
Mohit Yadav <myadav@companieshouse.gov.uk>
2024-10-21 12:41:59 +0100

    removing unused imports

--- src/test/java/uk/gov/companieshouse/lookup/controller/CompanyLookupControllerTest.java
+++ src/test/java/uk/gov/companieshouse/lookup/controller/CompanyLookupControllerTest.java
@@ -10,13 +10,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.mockito.Mockito.when;
 
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
