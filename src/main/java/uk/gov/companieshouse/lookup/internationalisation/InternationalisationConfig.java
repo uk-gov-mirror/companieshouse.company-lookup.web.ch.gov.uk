6e79cc38da84263b76fb7f4cda0c1c7f036ed5f8
hwatkins-ch <hwatkins@companieshouse.gov.uk>
2024-11-25 11:14:45 +0000

    remove: WebMVC annotation causing potential issues

--- src/main/java/uk/gov/companieshouse/lookup/internationalisation/InternationalisationConfig.java
+++ src/main/java/uk/gov/companieshouse/lookup/internationalisation/InternationalisationConfig.java
@@ -7,13 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
-@EnableWebMvc
 public class InternationalisationConfig implements WebMvcConfigurer {
 
     @Bean
